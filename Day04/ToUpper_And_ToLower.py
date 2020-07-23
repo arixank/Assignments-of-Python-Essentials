90677b662c9f322178c8efd7dc194ddd7096d8db
Compiler Bro's <50585400+arixank@users.noreply.github.com>
2020-07-23 17:31:07 +0530

    Day04 Assignments uploaded !!
    I'm very happy that I could make a very robust program !!! 
    Thank You !!

---
+++ Day04/ToUpper_And_ToLower.py
@@ -0,0 +1,11 @@
+print(" $$$$$$$$$$$$$$$ Program to convert string !! $$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+key = input("Enter a sentence: \n")
+
+lower = key.lower()
+
+print(f"Lower cased sentence: {lower} \n")
+
+upper = key.upper()
+
+print(f"Upper cased sentence: {upper} \n")
